71f15905b18f6fc044ba32d31c3f29bdadfa15f2
Светлана Краснова <skrasnova005@gmail.com>
2025-06-27 17:35:00 +0300

    Building collages added

--- Registration/run_ants.py
+++ Registration/run_ants.py
@@ -77,6 +77,25 @@ for pair in pairs:
     fixed_img, fixed_ds = load_dcm_to_array(fixed_path)
     moving_img, moving_ds = load_dcm_to_array(moving_path)
 
+    h1 = fixed_img.shape[0]
+    h2 = moving_img.shape[0]
+    
+    min_height = min(h1, h2)
+    
+    if h1 != min_height:
+        ratio = min_height / h1
+        new_width = int(fixed_img.shape[1] * ratio)
+        fixed_img = cv2.resize(fixed_img, 
+                               (new_width, min_height),
+                               interpolation=cv2.INTER_LANCZOS4)
+    
+    if h2 != min_height:
+        ratio = min_height / h2
+        new_width = int(moving_img.shape[1] * ratio)
+        moving_img = cv2.resize(moving_img,
+                                (new_width, min_height),
+                                interpolation=cv2.INTER_LANCZOS4)
+
     fixed_img = resize_image(fixed_img, scale_factor=0.5)
     moving_img = resize_image(moving_img, scale_factor=0.5)
 
@@ -95,6 +114,36 @@ for pair in pairs:
     fixed_info = parse_filename(fixed_path)
     output_path = f"{fixed_info['patient_id']}_{fixed_info['side']}_{fixed_info['projection']}_reg.png"
 
-    from PIL import Image
-    warped_uint8 = ((warped_np - warped_np.min()) / (warped_np.max() - warped_np.min()) * 255).astype(np.uint8)
-    Image.fromarray(warped_uint8).save(output_path)
+    from PIL import Image, ImageDraw,ImageFont
+    fixed = ((fixed_img - fixed_img.min()) / (fixed_img.max() - fixed_img.min()) * 255).astype(np.uint8)
+    fixed_img = Image.fromarray(fixed)
+    
+    moving_img = ((moving_img - moving_img.min()) / (moving_img.max() - moving_img.min()) * 255).astype(np.uint8)
+    moving_img = Image.fromarray(moving_img)
+    
+    warped = ((warped_np - warped_np.min()) / (warped_np.max() - warped_np.min()) * 255).astype(np.uint8)
+    warped_uint8 = Image.fromarray(warped)
+
+    collage = Image.new('RGB', (4 * fixed_img.width, fixed_img.height + 55), color=(255, 255, 255))
+    collage.paste(moving_img, (0, 0))
+    collage.paste(fixed_img, (fixed_img.width, 0))
+    collage.paste(warped_uint8, (fixed_img.width * 2, 0, fixed_img.width * 2 + warped_uint8.width, warped_uint8.height))
+    collage.paste(Image.fromarray(cv2.absdiff(fixed, warped)), (fixed_img.width * 3, 0, fixed_img.width * 3 + warped_uint8.width, warped_uint8.height))
+
+    draw = ImageDraw.Draw(collage)
+
+    try:
+        font = ImageFont.truetype("arial.ttf", 32)  # Windows
+    except:
+        font = ImageFont.load_default()  # Linux / macOS
+
+    labels = ["Moving (before)", "Fixed (after)", "Registered", "Difference"]
+
+    for i, label in enumerate(labels):
+        x = i * fixed_img.width + 10
+        y = fixed_img.height + 5
+        draw.text((x, y), label, fill="black", font=font)
+
+
+    collage.save(output_path)
+    print(f"Created collage: {output_path}")
